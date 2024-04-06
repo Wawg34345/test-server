8381b64d910e408757339eecb469a36c003a224d
Wawg34345 <andrey.dar1214@gmail.com>
2024-04-06 16:43:00 +0300

    it works

--- basic frontend/app.js
+++ basic frontend/app.js
@@ -1,11 +1,26 @@
-let getJson = fetch('http://localhost/simple%20application/backend/server.php');
+let response = await fetch('http://localhost/simple%20application/backend/server.php');
 
-getJson.then(
-    () => {
+if(response.ok){
+    let json = await response.json();
+    let table = document.createElement('table');
 
-    },
+    console.log(json);
+    document.body.prepend(table);
 
-    () => {
-        
+    for(let i = 0; i < json.length; i++){
+        let tr = document.createElement('tr');
+
+        table.appendChild(tr);
+
+        for(let j = 0; j < json[i].length; j++){
+            let td = document.createElement('td');
+            td.textContent = json[i][j];
+            tr.appendChild(td);
+        }
+
     }
-);
+}
+
+else{
+    throw new Error('connection failed');
+}
